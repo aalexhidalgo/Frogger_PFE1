68746c2ff256fd716bf9f93212a93e541a1df563
aalexhidalgo <91455208+aalexhidalgo@users.noreply.github.com>
2022-11-20 22:46:49 +0100

    20/11/2022

--- Assets/Scripts/AH_GameManager.cs
+++ Assets/Scripts/AH_GameManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class AH_GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI timeText;
+    private float TimeCounter;
+
+    public Image lifeImage;
+    public Sprite[] lifeSpriteArray;
+
+    private AH_PlayerController playerControllerScript;
+
     void Start()
     {
-        
+        playerControllerScript = FindObjectOfType<AH_PlayerController>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (playerControllerScript.active == true) //Te time doesn't start counting if we didn't move
+        {
+            UpdateTime();
+        }
+    }
+
+    public void UpdateTime()
+    {
+        TimeCounter += Time.deltaTime;
+        timeText.text = TimeCounter.ToString(); //With Mathf.Round we get the number without the miliseconds        
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
     }
 }
--- Assets/Scripts/AH_PlayerController.cs
+++ Assets/Scripts/AH_PlayerController.cs
@@ -4,15 +4,101 @@ using UnityEngine;
 
 public class AH_PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float spaceLimits = 4f;
+    private float distance = 1f; //Distance between steps
+    private Vector3 nextPos;
+
+    private bool isJumping;
+
+    private Animator playerAnimator;
+    private Rigidbody2D playerRigidbody;
+
+    public bool active;
+
+    public int lifeCounter = 3;
+
+    private AH_GameManager GameManagerScript;
+
+    void Awake()
     {
-        
+        playerAnimator = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        GameManagerScript = FindObjectOfType<AH_GameManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Player Movement
+
+        if (isJumping == false) //As a cooldown, we cannot press a key if we didn't arive to the new position
+        {
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                StartCoroutine(Movement());
+                nextPos.y = transform.position.y + distance;
+                transform.position = nextPos;
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+
+            if (Input.GetAxisRaw("Vertical") < 0 && transform.position.y != -spaceLimits)
+            {
+                StartCoroutine(Movement());
+                nextPos.y = transform.position.y - distance;
+                transform.position = nextPos;
+                transform.rotation = Quaternion.Euler(0, 0, -180);
+            }
+
+            if (Input.GetAxisRaw("Horizontal") > 0 && transform.position.x != spaceLimits)
+            {
+                StartCoroutine(Movement());
+                nextPos.x = transform.position.x + distance;
+                transform.position = nextPos;
+                transform.rotation = Quaternion.Euler(0, 0, -90);
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < 0 && transform.position.x != -spaceLimits)
+            {
+                StartCoroutine(Movement());
+                nextPos.x = transform.position.x - distance;
+                transform.position = nextPos;
+                transform.rotation = Quaternion.Euler(0, 0, 90);
+            }
+        }
+
     }
+
+    private void LateUpdate()
+    {
+        playerAnimator.SetBool("IsJumping", isJumping);
+    }
+
+    //Animation transition betweens steps
+    private IEnumerator Movement()
+    {
+        active = true;
+        isJumping = true;
+        yield return new WaitForSeconds(0.25f);
+        isJumping = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Equals("Enemy"))
+        {
+            lifeCounter--;
+
+            if(lifeCounter <= 0)
+            {
+                GameManagerScript.GameOver();
+            }
+        }
+
+    }
+
+    public void UpdateLife()
+    {
+        float currentImage = lifeCounter;
+        GameManagerScript.lifeImage.sprite = GameManagerScript.lifeSpriteArray[(int)currentImage];
+    }
+
 }
