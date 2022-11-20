1edb4dd331b6e97d0b204016b5401944b8825578
aalexhidalgo <91455208+aalexhidalgo@users.noreply.github.com>
2022-11-20 15:02:30 +0100

    MenuPanel 20/11/2022

---
+++ Assets/Scripts/AH_MenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AH_MenuManager : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitButton()
+    {
+        //Maria
+    }
+}
