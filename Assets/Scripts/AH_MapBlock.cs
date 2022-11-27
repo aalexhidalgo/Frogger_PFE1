a74d3e60ad53fcc56ad90242f94b05c04768d93a
aalexhidalgo <91455208+aalexhidalgo@users.noreply.github.com>
2022-11-27 19:06:53 +0100

    Map blocks catualizados

---
+++ Assets/Scripts/AH_MapBlock.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AH_MapBlock : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+}
--- Assets/Scripts/AH_MapGenerator.cs
+++ Assets/Scripts/AH_MapGenerator.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class AH_MapGenerator : MonoBehaviour
 {
-    void Start()
-    {
+    public List<AH_MapBlock> MapBlocks = new List<AH_MapBlock>(); //List with all the map blocks
+    public Transform GameStartPoint;
+    public List<AH_MapBlock> currentBlocks = new List<AH_MapBlock>(); //Currentblocks in the scene (clone)
 
+    private void Start()
+    {
+        GenerateMapBlocks();
     }
 
-    void Update()
+    public void AddMapBlock()
     {
+        //Random map block instance
+        int randomIndex = Random.Range(0, MapBlocks.Count);
+        AH_MapBlock currentBlock = (AH_MapBlock)Instantiate(MapBlocks[randomIndex]);
 
+        //currentBlock.transform.SetParent(this.transform, false);
+        Vector3 spawnPosition = Vector3.zero;
+
+        if (currentBlocks.Count == 0) //First point of spawn of the map blocks
+        {
+            spawnPosition = GameStartPoint.position;
+        }
+        else  //Every map block will appear attached to the end point of the previous map block
+        {
+            spawnPosition = currentBlocks[currentBlocks.Count - 1].endPoint.position;
+        }
+
+        currentBlock.transform.position = spawnPosition;
+        currentBlocks.Add(currentBlock);
+    }
+
+    public void GenerateMapBlocks() //Generates map blocks until it reaches 7
+    {
+        for (int i = 0; i < 7; i++) //cambiar número
+        {
+            AddMapBlock();
+        }
     }
 }
